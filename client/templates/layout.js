0733eecbb22eed2f30ab94de0a8fd5c1562bc6b2
Steffen Strätz <info@moooji.com>
2015-03-29 22:44:31 +0200

    Added slideout menu

---
+++ client/templates/layout.js
@@ -0,0 +1,23 @@
+var slideout;
+
+Template.layout.rendered = function () {
+
+    console.log("Layout rendered");
+    slideout = new Slideout({
+        'panel': document.getElementById('panel'),
+        'menu': document.getElementById('menu'),
+        'padding': 256,
+        'tolerance': 70
+    });
+};
+
+Template.layout.events({
+    'click #slideout-toggle-button': function onToggleSlideout(event, template) {
+        event.preventDefault();
+
+        if(slideout) {
+            console.log("Toggle Slideout");
+            slideout.toggle();
+        }
+    }
+});
