b6e2514e7d9e012248a45c38754188a9def795a9
Steffen Strätz <info@moooji.com>
2015-04-24 01:32:32 +0200

    Use TLS for HTTPS

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ function ensureTests () {
 function updateRunners () {
 
   var runners = [
-    { title: "CasperJS", slug: "casperjs", command: "casperjs test --ssl-protocol=tlsv1" },
+    { title: "CasperJS", slug: "casperjs", command: "casperjs any --ssl-protocol=tlsv1" },
     { title: "Mocha", slug: "mocha", command: "mocha" }
   ];
 
